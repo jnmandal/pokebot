a7aed300070b12a750ae64ec512449abb3002a9e
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-03-16 11:33:02 -0500

    move pokebot to separate file, add some reader methods pokebot hunt_for_pokemon not currently working

--- ocr_script.rb
+++ ocr_script.rb
@@ -2,6 +2,10 @@ require_relative 'pokereader.rb'
 
 pokereader = Pokereader.new(ARGV[0])
 
-if ARGV[1] == "appeared?"
+if ARGV[1] == "appeared"
   puts pokereader.in_battle?
+elsif ARGV[1] == "include"
+  puts pokereader.include?(ARGV[2])
+else
+  puts pokereader.text
 end
--- pokereader.rb
+++ pokereader.rb
@@ -4,10 +4,13 @@ class Pokereader
   def initialize(filepath)
     @image = RTesseract.new(filepath, :processor => "quick_magick", :lang => "eng")
   end
+  def include?(word)
+    @image.to_s.include?(word)
+  end
   def in_battle?
     @image.to_s.include?("appeared")
   end
+  def text
+    @image.to_s
+  end
 end
-
-
-
